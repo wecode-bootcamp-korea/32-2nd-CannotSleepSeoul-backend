2e01ff2f23f484db6ab9cff20756369629d3c92a
wdelight34 <wdelight34@gmail.com>
2022-05-11 16:23:23 +0900

    Fix: migrations

--- CSS/settings.py
+++ CSS/settings.py
@@ -42,7 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders', 
+    'cores',
+    'hotels',
+    'reservations',
+    'reviews',
+    'users'
 ]
 
 MIDDLEWARE = [
