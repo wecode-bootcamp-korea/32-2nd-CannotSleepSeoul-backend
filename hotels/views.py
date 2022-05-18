25bbaae20ed8789f8d46dd92791ef46016e120ce
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-05-18 15:12:23 +0900

    Mentor review

--- hotels/views.py
+++ hotels/views.py
@@ -1,26 +1,29 @@
 
 import json
-
-from django.shortcuts import render
+from datetime import datetime, timedelta
 
 from django.views     import View
 from django.http      import JsonResponse
-from django.db.models import Q
+from django.db.models import Q, Avg, Min
 
-from hotels.models    import Hotel, HotelImage, Location, Room, RoomType
+from hotels.models       import Hotel
+from reservations.models import Reservation
 
 class MainView(View):
     def get(self, request):
         try:
-            hotels = Hotel.objects.all()
+            hotels = Hotel.objects\
+                          .annotate(rating_avg=Avg('review__rating'))\
+                          .annotate(min_price=Min('room__room_type__price'))\
+                          .all()
 
             hotel_info = [{
                 'id'        : hotel.id,
                 'gu'        : hotel.location.gu,
-                'image_url' : hotel.hotelimage_set.filter(hotel_id=hotel.id)[0].image_url,
+                'image_url' : hotel.hotelimage_set.all()[0].image_url,
                 'name'      : hotel.name,
-                'price'     : min([room.room_type.price for room in hotel.room_set.all()]),
-                'rating'    : hotel.review_set.filter(hotel_id=hotel.id)[0].rating
+                'price'     : hotel.min_price,
+                'rating'    : hotel.rating_avg
             } for hotel in hotels]
 
             return JsonResponse({'hotel_info' : hotel_info}, status = 200)
@@ -32,32 +35,45 @@ class ListView(View):
         try:
             hotel_name = request.GET.get('hotelName', None)
             searching  = request.GET.get('locationGu', None)
-            page       = int(request.GET.get('page', 0))
+            offset     = int(request.GET.get('offset', 0))
             limit      = int(request.GET.get('limit', 30))
-            offset     = (page*limit)
             check_in   = request.GET.get('check_in', None)
             check_out  = request.GET.get('check_out', None)
-            reserved_rooms = []
             
+            reserved_rooms = [] 
+
+            if check_in and check_out:    
+                check_in  = datetime.strptime(check_in, '%Y-%m-%d')
+                check_out = datetime.strptime(check_out, '%Y-%m-%d')
+
+                q_reservation = Q(check_out__range=[check_in + timedelta(days=1), check_out]) | \
+                                Q(check_in__range=[check_in, check_out-timedelta(days=1)])
+
+                reserved_rooms = Reservation.objects.filter(q_reservation)
+
             q = Q()
 
             if hotel_name:
-                q &= Q(hotel_name=hotel_name)
+                q &= Q(hotel__name=hotel_name)
 
             if searching:
-                q &= Q(name_icontains=searching)
+                q &= Q(name__icontains=searching)
 
-            hotels = Hotel.objects.filter(q).order_by('?')[offset:offset+limit]
+            hotels = Hotel.objects\
+                          .annotate(rating_avg=Avg('review__rating'))\
+                          .annotate(min_price=Min('room__room_type__price'))\
+                          .exclude(room__reservation__in=reserved_rooms)\
+                          .filter(q).order_by('id')[offset:offset+limit]
 
             hotel_list = [{
                 'hotel_id'  : hotel.id,
                 'name'      : hotel.name,
-                'image'     : hotel.hotelimage_set.filter(hotel_id=hotel.id)[0].image_url,
+                'image'     : hotel.hotelimage_set.all()[0].image_url,
                 'city'      : hotel.location.city,
                 'gu'        : hotel.location.gu,
                 'ro'        : hotel.location.ro,
                 'detail'    : hotel.location.detail,
-                'price'     : min([room.room_type.price for room in hotel.room_set.all()]),
+                'price'     : hotel.min_price,
                 'latitude'  : hotel.latitude,
                 'longitude' : hotel.longitude,
             } for hotel in hotels]
